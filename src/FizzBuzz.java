b69c43a21743006fd6d2d740fa962e6f662cdf27
Domagoj <curic.domagoj993@gmail.com>
2025-03-24 19:34:10 +0100

    update dodan 101 broj u brojac

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
     public static void main (String[] args){
 
-        for(int i=0 ; i < 100; i++){
+        for(int i=0 ; i < 101; i++){
 
             if (i % 3 == 0 && i % 5 == 0){
                 if(i == 0){
