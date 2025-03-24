6a59801624a1a85a75dcc7fddb09f95562d9fd09
Domagoj <curic.domagoj993@gmail.com>
2025-03-24 19:04:08 +0100

    dodan zadatak drugi

--- src/temperatura.java
+++ src/temperatura.java
@@ -6,6 +6,20 @@ public class temperatura {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Unesi temperaturu: ");
+        double temperaturaC = scanner.nextDouble();
+
+
+        double Far = (temperaturaC * ((double) 9 /5) + 32);
+        double Kel = temperaturaC + 273.15;
+
+        System.out.println("temperatura u Faranheitim je " + Far);
+        System.out.println("temperatura u kelvinima je " + Kel);
+
+        scanner.close();
+
+
+
 
     }
 
